fdd0798e7dfa9779a7c7a087a1f9bd93a9572e81
Jeremie <jeremieflahaut@gmail.com>
2025-04-23 09:02:01 +0200

    fix cors

--- backend/app/config/cors.php
+++ backend/app/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => env('APP_ENV') === 'production' ? ['https://pea.dev-fullstack.net'] : ['http://localhost:3000'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
 
     'allowed_origins_patterns' => [],
 
